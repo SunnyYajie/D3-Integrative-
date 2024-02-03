557cce9067f4b5f8ef6dd3b017161a5cfaf52fa2
Eijay <eijay.gabrentina@sdca.edu.ph>
2024-02-03 15:14:54 +0800

    First commit

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
 /* ========================== Profession/Description Animation In Home Page ========================== */
 var typed = new Typed(".profession",{
-    strings:["" ,"Programmer","Web Designer", "Student", "Gamer"],
+    strings:["" ,"Fun","Enjoyment", "Laughter", "Friendship"],
     typeSpeed: 90,
     BackSpeed: 30,
     loop: true})
-
-/* ========================== Tab Function In About Section ========================== */
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
-/* ========================== Tab Function In Portfolio Section ========================== */
-var tablinks2 = document.getElementsByClassName("tab-links2");
-var tabcontents2 = document.getElementsByClassName("tab-contents2");
-
-function opentab2(tabname){
-    for(tablink of tablinks2){
-        tablink.classList.remove("active-link2");
-    }
-    for(tabcontent of tabcontents2){
-        tabcontent.classList.remove("active-tab2");
-    }
-    event.currentTarget.classList.add("active-link2");
-    document.getElementById(tabname).classList.add("active-tab2");
-}
 
 /* ========================== Carousel In Achievements Tab Located In Portfolio Section ========================== */
 const track = document.getElementById("slider");
